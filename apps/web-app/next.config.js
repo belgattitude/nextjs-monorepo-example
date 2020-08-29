7252cef6c400c3299539a6894ab80ae107dc5131
Sébastien Vanvelthem <belgattitude@gmail.com>
2020-08-29 23:03:28 +0200

    chore(next): vercel-build target server

--- apps/web-app/next.config.js
+++ apps/web-app/next.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const NEXTJS_BUILD_TARGET = process.env.NEXTJS_BUILD_TARGET || 'serverless';
 module.exports = {
-    //target: NEXTJS_BUILD_TARGET,
+    target: NEXTJS_BUILD_TARGET,
     webpack: function (config, { defaultLoaders }) {
         const resolvedBaseUrl = path.resolve(config.context, '../../')
         config.module.rules = [
