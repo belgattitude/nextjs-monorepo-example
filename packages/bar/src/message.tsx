af2d4f59ab5b7ad57250ce57d7e5ad5479dfb6e0
Sébastien Vanvelthem <belgattitude@gmail.com>
2020-08-31 15:54:22 +0200

    test with a red border

--- packages/bar/src/message.tsx
+++ packages/bar/src/message.tsx
@@ -2,9 +2,19 @@ import React from "react";
 //import styles from "./message.module.scss";
 
 type Props = {
+    borderColor?: string;
     message: string;
     children?: never;
 }
 
-export const Message: React.FC<Props> = (props) => <span>{props.message}</span>;
+const defaultProps = {
+    borderColor: 'red',
+}
+
+export const Message: React.FC<Props> = (props) => {
+    const {borderColor} = {...defaultProps, ...props};
+    return (
+        <div style={{display: 'inline-block', border: `1px solid ${borderColor}`}}>{props.message}</div>
+    );
+}
 
