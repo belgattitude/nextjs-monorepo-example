bc017f8d120d5d3c4297f00aea7099190186ca92
Sébastien Vanvelthem <belgattitude@gmail.com>
2020-09-04 09:55:32 +0200

    style

--- apps/blog-app/src/data/blog.ts
+++ apps/blog-app/src/data/blog.ts
@@ -1,13 +1,11 @@
 export type Post = {
-    slug: string,
-    title: string,
+  slug: string
+  title: string
 }
 
 const posts: Post[] = [
-    {slug: 'post1', title: 'The first blog post'},
-    {slug: 'post2', title: 'The second blog post'},
-];
+  { slug: 'post1', title: 'The first blog post' },
+  { slug: 'post2', title: 'The second blog post' },
+]
 
-export const getPosts = (): Post[] => posts;
-
-
+export const getPosts = (): Post[] => posts
