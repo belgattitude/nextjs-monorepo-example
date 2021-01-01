8862fe3dd993a38f8106e086a4bf711eff7be5dd
Sébastien Vanvelthem <belgattitude@gmail.com>
2021-01-01 12:45:19 +0100

    feat: add example for layout

--- apps/web-app/src/components/layout.tsx
+++ apps/web-app/src/components/layout.tsx
@@ -1,8 +1,6 @@
+import React from 'react'
+
 export const Layout: React.FC = (props) => {
-    const {children} = props;
-    return (
-      <div>
-          {children}
-      </div>
-    );
+  const { children } = props
+  return <div>{children}</div>
 }
