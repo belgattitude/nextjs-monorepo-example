2b5a2964e5ff460a230e928470c3e6ef049c2d92
Sébastien Vanvelthem <belgattitude@gmail.com>
2021-01-01 12:54:00 +0100

    lint: prettier all

--- apps/blog-app/src/components/layout.tsx
+++ apps/blog-app/src/components/layout.tsx
@@ -1,8 +1,4 @@
 export const Layout: React.FC = (props) => {
-    const {children} = props;
-    return (
-      <div>
-          {children}
-      </div>
-    );
+  const { children } = props
+  return <div>{children}</div>
 }
