dc660f8d76593566054ff14408c295012aa5cf06
Sébastien Vanvelthem <belgattitude@gmail.com>
2021-05-14 10:11:15 +0200

    ci: perf, allow disabling sourcemaps

--- apps/web-app/next.config.js
+++ apps/web-app/next.config.js
@@ -20,17 +20,24 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
+/**
+ * A way to allow CI optimization when the build done there is not used
+ * to deliver an image or deploy the files.
+ * @link https://nextjs.org/docs/advanced-features/source-maps
+ */
+const disableSourceMaps = process.env.NEXT_DISABLE_SOURCEMAPS === 'true';
+if (disableSourceMaps) {
+  console.log(
+    '[INFO]: Sourcemaps have been disabled through NEXT_DISABLE_SOURCEMAPS'
+  );
+}
+
 const config = withBundleAnalyzer(
   withTM({
     target: NEXTJS_BUILD_TARGET,
     reactStrictMode: true,
     future: { webpack5: true },
-    resolve: {
-      fallback: {
-        // Fixes npm packages that depend on `fs` module
-        fs: false,
-      },
-    },
+    productionBrowserSourceMaps: !disableSourceMaps,
     webpack: function (config, { defaultLoaders }) {
       const resolvedBaseUrl = path.resolve(config.context, '../../');
       // This extra config allows to use paths defined in tsconfig
