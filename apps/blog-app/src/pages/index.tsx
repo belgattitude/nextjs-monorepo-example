141222d1fe3252d5faee1b7af86fe3d91597960d
Sébastien Vanvelthem <belgattitude@gmail.com>
2021-05-14 18:19:55 +0200

    feat: next-seo example

--- apps/blog-app/src/pages/index.tsx
+++ apps/blog-app/src/pages/index.tsx
@@ -2,6 +2,7 @@ import { sayHello } from '@optional-package-scope/foo';
 import { getPosts, Post } from '../data/blog';
 import { MainLayout } from '@/components/layout/main-layout';
 import Image from 'next/image';
+import { NextSeo } from 'next-seo';
 
 type Props = {
   posts: Post[];
@@ -9,28 +10,34 @@ type Props = {
 
 export default function Blog({ posts }: Props) {
   return (
-    <MainLayout>
-      <h3>I'm the SSG blog-app</h3>
-      <ul>
-        <li>{`Foo says: ${sayHello(
-          'World'
-        )} from @optional-package-scope/foo`}</li>
-      </ul>
-      <h3>Here's the blog posts</h3>
-      <ul>
-        {posts.map(({ title, slug }) => (
-          <article key={slug} className="prose lg:prose-xl">
-            <p>{title}</p>
-          </article>
-        ))}
-      </ul>
-      <Image
-        src={'/images/nextjs-logo.png'}
-        alt={'logo'}
-        width={400}
-        height={240}
+    <>
+      <NextSeo
+        title="[blog-app] nextjs-monorepo example"
+        description="See https://github.com/belgattitude/nextjs-monorepo-example"
       />
-    </MainLayout>
+      <MainLayout>
+        <h3>I'm the SSG blog-app</h3>
+        <ul>
+          <li>{`Foo says: ${sayHello(
+            'World'
+          )} from @optional-package-scope/foo`}</li>
+        </ul>
+        <h3>Here's the blog posts</h3>
+        <ul>
+          {posts.map(({ title, slug }) => (
+            <article key={slug} className="prose lg:prose-xl">
+              <p>{title}</p>
+            </article>
+          ))}
+        </ul>
+        <Image
+          src={'/images/nextjs-logo.png'}
+          alt={'logo'}
+          width={400}
+          height={240}
+        />
+      </MainLayout>
+    </>
   );
 }
 
