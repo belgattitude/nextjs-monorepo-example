771818201360a57161d01b17b8cdfc93794cc1de
Sébastien Vanvelthem <belgattitude@gmail.com>
2021-05-17 00:48:57 +0200

    refactor: remove unused typings

--- packages/ui-lib/typings/global.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '*.module.css' {
-  const classes: { readonly [key: string]: string };
-  export default classes;
-}
-
-declare module '*.module.scss' {
-  const classes: { readonly [key: string]: string };
-  export default classes;
-}
