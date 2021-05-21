cde9b9f046a68cf4db0521213b66a604bf291291
Sébastien Vanvelthem <belgattitude@gmail.com>
2021-05-21 21:55:20 +0200

    chore: jest config small update

---
+++ packages/core-lib/config/jest/css-transform.js
@@ -0,0 +1,15 @@
+// Borrowed from https://github.com/facebook/create-react-app v4.0.1
+'use strict';
+
+// This is a custom Jest transformer turning style imports into empty objects.
+// http://facebook.github.io/jest/docs/en/webpack.html
+
+module.exports = {
+  process() {
+    return 'module.exports = {};';
+  },
+  getCacheKey() {
+    // The output is always the same.
+    return 'cssTransform';
+  },
+};
