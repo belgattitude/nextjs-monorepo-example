b318af115e30253103467c9fd5ee32b23f1163ba
Sébastien Vanvelthem <belgattitude@gmail.com>
2021-06-12 14:00:34 +0200

    chore(vercel): trying out cache for vercel

---
+++ apps/web-app/scripts/vercel-install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# Workaround for cache on vercel
+# @link https://github.com/yarnpkg/berry/issues/2900
+
+set -e
+
+BASEDIR=$(dirname $(dirname $(realpath "$0")))
+CACHE_PATH="${BASEDIR}/node_modules/.yarn/cache"
+
+cd "$BASEDIR"
+
+#echo $CACHE_PATH;
+#exit 1;
+
+if [ -d "$CACHE_PATH" ]; then
+  echo "Install with cache in ${CACHE_PATH}"
+  YARN_CACHE_FOLDER="$CACHE_PATH" yarn install;
+else
+  echo "Install and populate cache in ${CACHE_PATH}"
+  yarn install
+  YARN_CACHE_FOLDER="$CACHE_PATH" yarn install;
+fi
