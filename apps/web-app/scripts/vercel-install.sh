c44879e9fb76091c88690b5ebf6cbc0aa5dabf6c
Sébastien Vanvelthem <belgattitude@gmail.com>
2021-06-12 14:34:26 +0200

    chore(vercel): trying out cache for vercel

--- apps/web-app/scripts/vercel-install.sh
+++ apps/web-app/scripts/vercel-install.sh
@@ -5,18 +5,18 @@
 set -e
 
 BASEDIR=$(dirname $(dirname $(realpath "$0")))
-CACHE_PATH="${BASEDIR}/node_modules/.yarn/cache"
+#CACHE_PATH="${BASEDIR}/node_modules/.yarn/cache"
+CACHE_PATH="${BASEDIR}/.next/cache/yarn-cache"
 
 cd "$BASEDIR"
-ls -la $CACHE_PATH
-#echo $CACHE_PATH;
-#exit 1;
 
-if [ -d "$CACHE_PATH" ]; then
-  echo "Install with cache in ${CACHE_PATH}"
-  YARN_CACHE_FOLDER="$CACHE_PATH" yarn install;
-else
-  echo "Install and populate cache in ${CACHE_PATH}"
-  yarn install
-  YARN_CACHE_FOLDER="$CACHE_PATH" yarn install;
-fi
+YARN_CACHE_FOLDER="$CACHE_PATH" yarn install
+
+#if [ -d "$CACHE_PATH" ]; then
+#  echo "Install with cache in ${CACHE_PATH}"
+#  YARN_CACHE_FOLDER="$CACHE_PATH" yarn install;
+#else
+#  echo "Install and populate cache in ${CACHE_PATH}"
+#  yarn install
+#  YARN_CACHE_FOLDER="$CACHE_PATH" yarn install;
+#fi
