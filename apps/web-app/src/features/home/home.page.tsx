6dea1aa39faa71d53f0681034c29cf719836d94d
Sébastien Vanvelthem <belgattitude@gmail.com>
2021-06-30 21:07:01 +0200

    feat: added common config for i18n namespaces

--- apps/web-app/src/features/home/home.page.tsx
+++ apps/web-app/src/features/home/home.page.tsx
@@ -5,18 +5,20 @@ import { HeroSection } from './sections/hero-section';
 import { FeaturesSection } from './sections/features-section';
 import { CTASection } from './sections/cta-section';
 import { useTranslation } from 'next-i18next';
+import { homeConfig } from './home.config';
+import { CustomTypeOptions } from 'react-i18next';
 
 type Props = {
   children?: never;
 };
 
 export const HomePage: React.FC<Props> = () => {
-  const { t } = useTranslation('home');
+  const { t } = useTranslation(homeConfig.i18nNamespaces);
 
   return (
     <>
       <NextSeo
-        title={t('page.title')}
+        title={t('home:page.title')}
         description="See https://github.com/belgattitude/nextjs-monorepo-example"
       />
       <MainLayout>
