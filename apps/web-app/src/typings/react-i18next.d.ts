361c4d23d2d302545cbebdf0888a9bd59e949c3a
Sébastien Vanvelthem <belgattitude@gmail.com>
2021-07-05 21:58:58 +0200

    feat(typing): add demo namespace

--- apps/web-app/src/typings/react-i18next.d.ts
+++ apps/web-app/src/typings/react-i18next.d.ts
@@ -7,6 +7,7 @@
 import 'react-i18next';
 import common from '../../public/locales/en/common.json';
 import home from '../../public/locales/en/home.json';
+import demo from '../../public/locales/en/demo.json';
 
 declare module 'react-i18next' {
   interface CustomTypeOptions {
@@ -14,6 +15,7 @@ declare module 'react-i18next' {
     resources: {
       common: typeof common;
       home: typeof home;
+      demo: typeof demo;
     };
   }
 }
