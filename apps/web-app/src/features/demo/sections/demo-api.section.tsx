f7664a66a199cb7bf2c64dbc5f411abc7493201a
Sébastien Vanvelthem <belgattitude@gmail.com>
2021-07-10 22:47:54 +0200

    feat: playing with design

--- apps/web-app/src/features/demo/sections/demo-api.section.tsx
+++ apps/web-app/src/features/demo/sections/demo-api.section.tsx
@@ -1,48 +1,85 @@
-import axios from 'axios';
 import { useQuery } from 'react-query';
-import {
-  isJsonApiSuccessResponse,
-  JsonApiResponse,
-} from '@your-org/core-lib/api/json-api';
-import ky from 'ky';
+import { fetchPostsWithKy } from '../api/fetch-posts-ky.api';
 import { GetPosts } from '../../api/rest/post-repository.ssr';
+import styled from '@emotion/styled';
+import { css } from '@emotion/react';
 
 type Props = {
   children?: never;
 };
 
-const fetchPostWithAxios = async (): Promise<GetPosts> => {
-  return axios
-    .get<JsonApiResponse<GetPosts>>('/api/rest/post', {
-      responseType: 'json',
-    })
-    .then((resp) => {
-      const data = resp.data;
-      if (!isJsonApiSuccessResponse(data)) {
-        throw new Error(`Error fetching posts: ${data.errors}`);
-      }
-      return data.data;
-    });
+const BlogCtn = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: flex-end;
+  justify-content: center;
+  padding: 0.5rem;
+  text-align: center;
+  font-size: 1.2rem;
+  background-color: #333333;
+  color: #d9d9d9;
+  img {
+    width: 100%;
+    transition: transform 0.5s ease-in-out;
+    &:hover {
+      transform: scale(1.07);
+    }
+  }
+  div {
+    position: absolute;
+    max-width: 400px;
+    margin: 0;
+    padding: 0.5rem;
+    word-break: break-word;
+    font-size: 1.25rem;
+    background-color: rgba(0, 0, 0, 0.3);
+    color: #f2f2f2;
+
+    &:hover {
+      background-color: rgba(0, 0, 0, 0.5);
+      color: #e6e6e6;
+      transition: all ease 0.5s;
+    }
+  }
+`;
+
+const Blog: React.FC<{ post: GetPosts[number]; children?: never }> = (
+  props
+) => {
+  const { post } = props;
+  return (
+    <BlogCtn>
+      <img src={post.image ?? ''} alt={'cool'} />
+      <div>
+        <h3>{post.title}</h3>
+      </div>
+    </BlogCtn>
+  );
 };
 
-const fetchPostWithKy = async (): Promise<GetPosts> => {
-  return ky
-    .get('/api/rest/post', {
-      throwHttpErrors: true,
-    })
-    .json<JsonApiResponse<GetPosts>>()
-    .then((resp) => {
-      if (!isJsonApiSuccessResponse(resp)) {
-        throw new Error(`Error fetching posts: ${resp.errors}`);
-      }
-      return resp.data;
-    });
+const BlogListCtn = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  grid-gap: 1.5rem;
+  justify-items: center;
+  padding: 0;
+`;
+
+const BlogList: React.FC<{ posts: GetPosts; children?: never }> = (props) => {
+  const { posts } = props;
+  return (
+    <BlogListCtn>
+      {posts.map((post) => (
+        <Blog key={post.id} post={post} />
+      ))}
+    </BlogListCtn>
+  );
 };
 
 const ReactQueryApi: React.FC = () => {
   const { data, isLoading, error } = useQuery(
     'posts',
-    () => fetchPostWithKy(),
+    () => fetchPostsWithKy(),
     {}
   );
   if (isLoading) {
@@ -51,19 +88,15 @@ const ReactQueryApi: React.FC = () => {
   if (error) {
     return <div>Error {JSON.stringify(error)}</div>;
   }
-  return (
-    <div>
-      {data &&
-        data.map((post) => {
-          return <img src={post.image ?? ''} alt={'forest'} />;
-        })}
-    </div>
-  );
+  return <div>{data && <BlogList posts={data} />}</div>;
 };
 
 export const DemoApiSection: React.FC<Props> = () => {
   return (
-    <div>
+    <div
+      css={css`
+        width: 100px;
+      `}>
       <ReactQueryApi />
     </div>
   );
