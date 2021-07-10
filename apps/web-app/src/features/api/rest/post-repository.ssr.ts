bb0c22e1f5a8911dc560f67f250445db913b0c98
Sébastien Vanvelthem <belgattitude@gmail.com>
2021-07-10 20:49:07 +0200

    feat: example of post repository for ssr + types

---
+++ apps/web-app/src/features/api/rest/post-repository.ssr.ts
@@ -0,0 +1,41 @@
+import { prismaClient, PrismaClient } from '@your-org/db-main-prisma';
+import { InternalServerError } from '@tsed/exceptions';
+import { UnPromisify } from '@your-org/core-lib';
+
+export type GetPosts = UnPromisify<
+  ReturnType<typeof PostRepositorySsr['prototype']['getPosts']>
+>;
+
+export class PostRepositorySsr {
+  constructor(private prisma: PrismaClient) {}
+
+  /**
+   * @throws Exception
+   */
+  getPosts = async (options?: { limit?: number; offset?: number }) => {
+    const { limit = undefined, offset = undefined } = options ?? {};
+    try {
+      return await prismaClient.post.findMany({
+        skip: offset,
+        take: limit,
+        where: {
+          publishedAt: {
+            not: null,
+          },
+        },
+        include: {
+          author: {
+            select: {
+              firstName: true,
+              lastName: true,
+              nickname: true,
+            },
+          },
+        },
+        orderBy: { publishedAt: 'desc' },
+      });
+    } catch (e) {
+      throw new InternalServerError(`Posts can't be retrieved`, e);
+    }
+  };
+}
