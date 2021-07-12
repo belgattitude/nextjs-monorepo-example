2dac42df69b9d20baad2ed60fea7f6be13fe764c
Sébastien Vanvelthem <belgattitude@gmail.com>
2021-07-12 22:53:56 +0200

    feat(nextjs): example for next-image setup

--- apps/web-app/next.config.js
+++ apps/web-app/next.config.js
@@ -72,6 +72,15 @@ const config = withBundleAnalyzer(
     i18n,
     optimizeFonts: true,
 
+    // @link https://nextjs.org/docs/basic-features/image-optimization
+    images: {
+      deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+      imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+      disableStaticImages: false,
+      // Allowed domains for next/image
+      domains: ['source.unsplash.com'],
+    },
+
     eslint: {
       ignoreDuringBuilds: NEXTJS_IGNORE_ESLINT,
       dirs: ['src'],
