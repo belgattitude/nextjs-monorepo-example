ba530b8660e9c8e631f597470c79a159bc7d678a
Sébastien Vanvelthem <belgattitude@gmail.com>
2021-08-12 22:53:43 +0200

    refactor: prep multi prisma connection initial support

--- apps/web-app/src/backend/api/rest/poem-repository.ssr.ts
+++ apps/web-app/src/backend/api/rest/poem-repository.ssr.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@your-org/db-main-prisma';
+import { PrismaClientDbMain } from '@your-org/db-main-prisma';
 import { InternalServerError } from '@tsed/exceptions';
 import { UnPromisify } from '@your-org/core-lib';
 
@@ -7,7 +7,7 @@ export type GetPoems = UnPromisify<
 >;
 
 export class PoemRepositorySsr {
-  constructor(private prisma: PrismaClient) {}
+  constructor(private prisma: PrismaClientDbMain) {}
 
   /**
    * @throws Error
