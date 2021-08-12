a67b124062c861c2cb4bb3d187eafe1967cb4d24
Sébastien Vanvelthem <belgattitude@gmail.com>
2021-08-12 23:29:18 +0200

    feat: typesafe version of features config files

--- apps/web-app/src/features/demo/demo.config.ts
+++ apps/web-app/src/features/demo/demo.config.ts
@@ -1,4 +1,5 @@
-export const demoConfig = {
-  /** Namespaces that should be loaded for this page */
+import { FeaturesConfig } from '@/config/features.config';
+
+export const demoConfig: FeaturesConfig = {
   i18nNamespaces: ['common', 'demo'],
 } as const;
--- apps/web-app/src/features/home/home.config.ts
+++ apps/web-app/src/features/home/home.config.ts
@@ -1,9 +1,6 @@
-import { CustomTypeOptions } from 'react-i18next';
+import { FeaturesConfig } from '@/config/features.config';
 
-export const homeConfig = {
+export const homeConfig: FeaturesConfig = {
   /** Namespaces that should be loaded for this page */
-  i18nNamespaces: [
-    'common',
-    'home',
-  ] as (keyof CustomTypeOptions['resources'])[],
+  i18nNamespaces: ['common', 'home'],
 } as const;
