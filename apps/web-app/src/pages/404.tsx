a9afa2fedbd946593d204735ca9c118bddc67386
Sébastien Vanvelthem <belgattitude@gmail.com>
2021-08-25 22:28:25 +0200

    feat: translated 404 page

--- apps/web-app/src/pages/404.tsx
+++ apps/web-app/src/pages/404.tsx
@@ -1,32 +1,13 @@
 import { GetStaticPropsContext, InferGetStaticPropsType } from 'next';
 import { useTranslation } from 'react-i18next';
-import { I18nNamespaces } from '@/core/i18n/i18n-namespaces.type';
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import nextI18nextConfig from '../../next-i18next.config';
+import { getServerSideTranslations } from '@/core/i18n/get-server-side-translations';
 
-const i18nNamespaces: I18nNamespaces = ['servicePages'];
+const i18nNamespaces: Array<'systemPages'> = ['systemPages'];
 
 export const getStaticProps = async (context: GetStaticPropsContext) => {
   const { locale = 'en' } = context;
 
-  // Works (so it's not linked to nextjs apparently)
-  const getFakeServerSideTranslations = async (
-    _locale: string,
-    _namespaces: string[]
-  ) => {
-    return Promise.resolve({ aProperty: 'cool' });
-  };
-
-  // Dont work
-  // Error: ReactDOMServer does not yet support Suspense.
-  const getRealServerSideTranslations = async (
-    locale: string,
-    namespaces: string[]
-  ) => {
-    return await serverSideTranslations(locale, namespaces, nextI18nextConfig);
-  };
-
-  const inlinedTranslation = await getRealServerSideTranslations(
+  const inlinedTranslation = await getServerSideTranslations(
     locale,
     i18nNamespaces
   );
@@ -43,5 +24,5 @@ export default function Custom404(
   _props: InferGetStaticPropsType<typeof getStaticProps>
 ) {
   const { t } = useTranslation(i18nNamespaces);
-  return <h1>{t('servicePages:notFound.title')}</h1>;
+  return <h1>{t('systemPages:notFound.title')}</h1>;
 }
