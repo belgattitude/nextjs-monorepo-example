bdbfe277b6294703d8de5f5a3f8691ed2eb629ef
Sébastien Vanvelthem <belgattitude@gmail.com>
2021-08-25 21:12:42 +0200

    chore(testing): add i18n provider to test-utils

---
+++ apps/web-app/config/jest/i18next-stub.config.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { initReactI18next } from 'react-i18next';
+import { I18nextProvider } from 'react-i18next';
+import i18n from 'i18next';
+
+/**
+ * Using stub strategy, you can use mocks as well
+ * @link {https://react.i18next.com/misc/testing}
+ */
+i18n.use(initReactI18next).init({
+  lng: 'en',
+  fallbackLng: 'en',
+  ns: ['common'],
+  defaultNS: 'translations',
+  debug: false,
+  interpolation: {
+    escapeValue: false, // not needed for react!!
+  },
+  // Let empty so you can test for keys rather
+  // than translations
+  resources: { en: { translations: {} } },
+});
+
+export const I18nextTestStubProvider: React.FC = ({ children }) => {
+  return <I18nextProvider i18n={i18n}>{children}</I18nextProvider>;
+};
