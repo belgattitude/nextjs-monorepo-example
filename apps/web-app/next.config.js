56f3420e39d0044f59404082d9d7e5eef68409fa
Sébastien Vanvelthem <belgattitude@gmail.com>
2021-08-28 23:31:28 +0200

    chore(config): remove nextjs hack for prisma undici

--- apps/web-app/next.config.js
+++ apps/web-app/next.config.js
@@ -124,14 +124,6 @@ const nextConfig = {
 
   // @ts-ignore
   webpack: (config, { defaultLoaders, isServer }) => {
-    // A temp workaround for https://github.com/prisma/prisma/issues/6899#issuecomment-849126557
-    /*
-    if (isServer) {
-      config.externals.push('_http_common');
-    }
-
-     */
-
     config.module.rules.push({
       test: /\.svg$/,
       issuer: /\.(js|ts)x?$/,
