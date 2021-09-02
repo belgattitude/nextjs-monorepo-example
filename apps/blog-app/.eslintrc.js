ec692ac8d29df75282cef3bd6b2ecd216eaeff54
Sébastien Vanvelthem <belgattitude@gmail.com>
2021-09-02 10:06:17 +0200

    chore(eslint): add plugin import and move to js config

---
+++ apps/blog-app/.eslintrc.js
@@ -0,0 +1,54 @@
+module.exports = {
+  root: true,
+  ignorePatterns: ['node_modules/*', '.next'],
+  extends: [
+    '../../.eslintrc.base.js',
+    // Add specific rules for react and nextjs
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:@next/next/recommended',
+    'plugin:testing-library/react',
+  ],
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  rules: {
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'jsx-a11y/anchor-is-valid': 'off',
+
+    // next/image might not be yet a good move as of NextJs v11.
+    // https://github.com/vercel/next.js/discussions/16832
+    '@next/next/no-img-element': 'off',
+  },
+  overrides: [
+    {
+      files: ['next.config.js'],
+      parser: 'espree',
+      parserOptions: {
+        ecmaVersion: 2020,
+      },
+      rules: {
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-var-requires': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+      },
+    },
+    {
+      files: ['src/pages/**/*.ts', 'src/pages/**/*.tsx'],
+      rules: {
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+      },
+    },
+    {
+      files: ['src/backend/api/**/*.ts'],
+      rules: {
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+      },
+    },
+  ],
+};
