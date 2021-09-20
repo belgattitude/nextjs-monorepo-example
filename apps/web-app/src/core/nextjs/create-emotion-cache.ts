7e1b0c61c789b08cdcfffbc5659a82f67c555388
Sébastien Vanvelthem <belgattitude@gmail.com>
2021-09-20 16:41:00 +0200

    feat: custom emotion cache

--- apps/web-app/src/core/nextjs/create-emotion-cache.ts
+++ apps/web-app/src/core/nextjs/create-emotion-cache.ts
@@ -1,5 +1,5 @@
 import createCache from '@emotion/cache';
-import type { EmotionCache } from '@emotion/utils';
+import type { EmotionCache } from '@emotion/cache';
 
 export const createEmotionCache = (): EmotionCache => {
   return createCache({ key: 'css' });
