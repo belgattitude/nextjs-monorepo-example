cb1714ce6067f92c71a1538e04b79de22550d913
Sébastien Vanvelthem <belgattitude@gmail.com>
2021-09-21 18:05:13 +0200

    feat: demo with mui v5.0.0

--- apps/web-app/src/features/demo/blocks/demo-api.block.tsx
+++ apps/web-app/src/features/demo/blocks/demo-api.block.tsx
@@ -25,10 +25,10 @@ export const DemoApiBlock: React.FC<NoChildrenProps> = () => {
   return (
     <div>
       <div className="lg:container lg:mx-auto">
-        <h1 className="font-bold text-4xl mb-2">Some poems</h1>
+        <h1 className="font-bold text-4xl mb-2">Poetry on the wild.</h1>
         <h2 className="font-bold text-xl mb-2 text-indigo-600">
-          Regular client fetch with react-query and ky from nextjs api, db in
-          supabase.io, connection with prisma. Ui with tailwind
+          Client fetch with ky / react-query from nextjs api, db in supabase.io,
+          connection with prisma. Ui with tailwind
         </h2>
         <PoemGridWithReactQueryAndKy />
       </div>
