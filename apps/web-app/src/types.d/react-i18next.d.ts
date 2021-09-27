72de52e78c67476fc71c30948181e642f84afb82
Sébastien Vanvelthem <belgattitude@gmail.com>
2021-09-27 22:03:07 +0200

    fix: hack for react-i18next type augmentation
    Since i18next v21.0.0 typescript started to
    complain about a missing reportNamespace
    when using react i18next provider.
    
    Some comments available here:
    
    https://github.com/i18next/react-i18next/issues/1379

--- apps/web-app/src/types.d/react-i18next.d.ts
+++ apps/web-app/src/types.d/react-i18next.d.ts
@@ -5,10 +5,20 @@
  * @link https://react.i18next.com/latest/typescript
  */
 import 'react-i18next';
+import { ReportNamespaces } from 'react-i18next';
 import common from '../../public/locales/en/common.json';
 import demo from '../../public/locales/en/demo.json';
 import home from '../../public/locales/en/home.json';
 import system from '../../public/locales/en/system.json';
+
+// For an obscure reason i18n type augmentation present in react-i18next/ts4.1/index.d.ts
+// (and/or react-i18next/index.d.ts) aren't considered when using I18nextProvider
+// @see https://github.com/i18next/react-i18next/issues/1379
+declare module 'i18next' {
+  interface i18n {
+    reportNamespaces: ReportNamespaces;
+  }
+}
 
 declare module 'react-i18next' {
   interface CustomTypeOptions {
