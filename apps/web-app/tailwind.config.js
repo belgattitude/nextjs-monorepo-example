99bababbe1d50a404fe0f3659493eade15179553
Sébastien Vanvelthem <belgattitude@gmail.com>
2021-10-04 23:48:34 +0200

    fix(tailwind): explicit mode: jit is still needed

--- apps/web-app/tailwind.config.js
+++ apps/web-app/tailwind.config.js
@@ -2,6 +2,7 @@ const colors = require('tailwindcss/colors');
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
+  mode: 'jit',
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
