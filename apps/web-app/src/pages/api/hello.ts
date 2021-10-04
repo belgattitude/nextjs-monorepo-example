2080b97f04936355c63d1397bc99cb86cc4cfbf3
Sébastien Vanvelthem <belgattitude@gmail.com>
2021-10-04 22:45:39 +0200

    fix: no anonymous functions

--- apps/web-app/src/pages/api/hello.ts
+++ apps/web-app/src/pages/api/hello.ts
@@ -1,6 +1,9 @@
 import { sayHello } from '@your-org/core-lib';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-export default async function (req: NextApiRequest, res: NextApiResponse) {
+export default async function ApiHelloRoute(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   return res.send(sayHello('world loaded from /api/hello'));
 }
