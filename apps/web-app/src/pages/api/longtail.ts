cd74975caac414a6b7c5f8b784768103578b5b88
Sébastien Vanvelthem <belgattitude@gmail.com>
2021-10-08 15:00:40 +0200

    feat: graphql

---
+++ apps/web-app/src/pages/api/longtail.ts
@@ -0,0 +1,9 @@
+import { sayHello } from '@your-org/core-lib';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function LongtailApiRoute(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  return res.send(sayHello('world loaded from /api/hello'));
+}
