fe262d011845ac4d9471b3334b4bd387d96b2e87
Sébastien Vanvelthem <belgattitude@gmail.com>
2021-10-13 15:39:38 +0200

    chore: optimie svg by default on build

---
+++ .changeset/young-mails-arrive.md
@@ -0,0 +1,6 @@
+---
+'blog-app': minor
+'web-app': minor
+---
+
+Optimize svg with svgo by default when building the apps
--- apps/web-app/next.config.js
+++ apps/web-app/next.config.js
@@ -139,7 +139,24 @@ const nextConfig = {
     config.module.rules.push({
       test: /\.svg$/,
       issuer: /\.(js|ts)x?$/,
-      use: ['@svgr/webpack'],
+      use: [
+        {
+          loader: '@svgr/webpack',
+          // https://react-svgr.com/docs/webpack/#passing-options
+          options: {
+            svgo: true,
+            // @link https://github.com/svg/svgo#configuration
+            svgoConfig: {
+              multipass: false,
+              datauri: 'base64',
+              js2svg: {
+                indent: 2,
+                pretty: false,
+              },
+            },
+          },
+        },
+      ],
     });
 
     return config;
