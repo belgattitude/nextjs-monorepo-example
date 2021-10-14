757cab386d2c8274b5a68c91c5b4174eede8d2fa
Sébastien Vanvelthem <belgattitude@gmail.com>
2021-10-14 21:59:14 +0200

    chore(jest): cleanup config

--- packages/db-main-prisma/e2e/jest.config.js
+++ packages/db-main-prisma/e2e/jest.config.js
@@ -2,8 +2,6 @@
 'use strict';
 
 const { defaults: tsjPreset } = require('ts-jest/presets');
-const { pathsToModuleNameMapper } = require('ts-jest/utils');
-const { compilerOptions } = require('./tsconfig.e2e.json');
 
 /** @typedef {import('ts-jest/dist/types')} */
 /** @type {import('@jest/types').Config.InitialOptions} */
@@ -16,14 +14,8 @@ const config = {
   transform: {
     ...tsjPreset.transform,
   },
-  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|ts)$'],
   rootDir: '../',
   testMatch: ['<rootDir>/e2e/suites/**/*.test.ts'],
-  /**
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
-    prefix: '<rootDir>/',
-  }),
-   */
   globals: {
     'ts-jest': {
       diagnostics: true,
