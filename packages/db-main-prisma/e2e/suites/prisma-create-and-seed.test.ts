cdfd3a3446785cdd81cfba2ab053b3d353bc97f3
Sébastien Vanvelthem <belgattitude@gmail.com>
2021-10-14 18:31:57 +0200

    test(e2e): test on postgresql 13 & 14

--- packages/db-main-prisma/e2e/suites/prisma-create-and-seed.test.ts
+++ packages/db-main-prisma/e2e/suites/prisma-create-and-seed.test.ts
@@ -29,43 +29,45 @@ const execCmd = async (
 };
 
 describe('prisma cli commands', () => {
-  describe('Create and seed', () => {});
-  it('should load seed data in a newly created db', async () => {
-    const ctn = await E2eDockerContainers.startPostgresqlContainer(
-      'postgresql13'
-    );
-    const { dsn } = ctn;
+  describe('Create and seed', () => {
+    it.each(['postgresql13', 'postgresql14'] as const)(
+      'should load seed data in a newly created db',
+      async (dbKey) => {
+        const ctn = await E2eDockerContainers.startPostgresqlContainer(dbKey);
+        const { dsn } = ctn;
 
-    const options: ExecSyncOptionsWithStringEncoding = {
-      encoding: 'utf-8',
-      env: {
-        PRISMA_DATABASE_URL: dsn,
-      },
-    };
-
-    const createResult = await execCmd('yarn prisma db push', options);
-
-    expect(createResult.status).toBeUndefined();
-    expect(createResult.stdout).toMatch(
-      /your database is now in sync with your schema/i
-    );
-
-    const seedResult = await execCmd('yarn prisma db seed', options);
-
-    expect(seedResult.status).toBeUndefined();
-    expect(seedResult.stdout).toMatch(/seeding finished/i);
-
-    const prisma = PrismaManager.getDevSafeInstance('test', () => {
-      return new PrismaClientDbMain({
-        datasources: {
-          db: {
-            url: dsn,
+        const options: ExecSyncOptionsWithStringEncoding = {
+          encoding: 'utf-8',
+          env: {
+            PRISMA_DATABASE_URL: dsn,
           },
-        },
-      });
-    });
+        };
 
-    const poetry = await prisma.poem.findFirst();
-    expect(poetry).toBeDefined();
+        const createResult = await execCmd('yarn prisma db push', options);
+
+        expect(createResult.status).toBeUndefined();
+        expect(createResult.stdout).toMatch(
+          /your database is now in sync with your schema/i
+        );
+
+        const seedResult = await execCmd('yarn prisma db seed', options);
+
+        expect(seedResult.status).toBeUndefined();
+        expect(seedResult.stdout).toMatch(/seeding finished/i);
+
+        const prisma = PrismaManager.getDevSafeInstance('test', () => {
+          return new PrismaClientDbMain({
+            datasources: {
+              db: {
+                url: dsn,
+              },
+            },
+          });
+        });
+
+        const poetry = await prisma.poem.findFirst();
+        expect(poetry).toBeDefined();
+      }
+    );
   });
 });
