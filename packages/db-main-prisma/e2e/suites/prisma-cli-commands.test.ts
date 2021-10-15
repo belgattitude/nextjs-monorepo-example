8b7826517cd94dad4b515212a75e9b687a2fd3a8
Sébastien Vanvelthem <belgattitude@gmail.com>
2021-10-15 16:40:23 +0200

    test(e2e): refactor to use execa

--- packages/db-main-prisma/e2e/suites/prisma-cli-commands.test.ts
+++ packages/db-main-prisma/e2e/suites/prisma-cli-commands.test.ts
@@ -1,59 +1,35 @@
-import {
-  execSync,
-  ExecSyncOptionsWithStringEncoding,
-  SpawnSyncReturns,
-} from 'child_process';
 import { PrismaManager, PrismaClientDbMain } from '@your-org/db-main-prisma';
+import execa from 'execa';
+import type { Options as ExecaOptions } from 'execa';
 import { E2eDockerContainers } from '../test-containers';
 
-// Quick util
-const execCmd = async (
-  cmd: string,
-  options: ExecSyncOptionsWithStringEncoding
-): Promise<{
-  stdout?: string;
-  stderr?: string;
-  status?: number | null;
-}> => {
-  try {
-    const stdout = await execSync(cmd, options);
-    return {
-      stdout,
-    };
-  } catch (e: unknown) {
-    const error = e as unknown as Error & SpawnSyncReturns<Buffer>;
-    return {
-      status: error.status,
-    };
-  }
-};
-
 describe('prisma cli commands', () => {
-  describe('Create and seed', () => {
+  describe('yarn prisma db create and seed', () => {
     it.each(['postgresql13', 'postgresql14'] as const)(
       'should load seed data in a newly created db',
       async (dbKey) => {
         const ctn = await E2eDockerContainers.startPostgresqlContainer(dbKey);
         const { dsn } = ctn;
 
-        const options: ExecSyncOptionsWithStringEncoding = {
-          encoding: 'utf-8',
+        const options: ExecaOptions = {
+          //encoding: 'utf-8',
+          shell: true,
           env: {
             ...process.env,
             PRISMA_DATABASE_URL: dsn,
           },
         };
 
-        const createResult = await execCmd('yarn prisma db push', options);
+        const createResult = await execa('yarn prisma db push', options);
 
-        expect(createResult.status).toBeUndefined();
+        expect(createResult.exitCode).toStrictEqual(0);
         expect(createResult.stdout).toMatch(
           /your database is now in sync with your schema/i
         );
 
-        const seedResult = await execCmd('yarn prisma db seed', options);
+        const seedResult = await execa('yarn prisma db seed', options);
 
-        expect(seedResult.status).toBeUndefined();
+        expect(seedResult.exitCode).toStrictEqual(0);
         expect(seedResult.stdout).toMatch(/seeding finished/i);
 
         const prisma = PrismaManager.getDevSafeInstance('test', () => {
