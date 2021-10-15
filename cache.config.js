16907008a2d3a467b3381f03ab36125fdb8642aa
Sébastien Vanvelthem <belgattitude@gmail.com>
2021-10-15 13:12:09 +0200

    feat: introduce global cache

---
+++ cache.config.js
@@ -0,0 +1,39 @@
+/**
+ * Convenience script to harmonize cache directories across various
+ * tooling such as eslint and jest
+ */
+// @ts-check
+'use strict';
+
+const { resolve } = require('path');
+
+const globalCachePath = resolve(`${__dirname}/.cache`);
+
+/**
+ * @param {string} packageName
+ * @returns string
+ */
+function sanitize(packageName) {
+  return packageName.replace(/[^a-z0-9\/.@_-]+/gi, '_');
+}
+
+/**
+ * @param {string} packageName
+ * @returns string
+ */
+function getEslintCachePath(packageName) {
+  return `${globalCachePath}/${sanitize(packageName)}/eslint`;
+}
+
+/**
+ * @param {string} packageName
+ * @returns string
+ */
+function getJestCachePath(packageName) {
+  return `${globalCachePath}/${sanitize(packageName)}/jest`;
+}
+
+module.exports = {
+  getJestCachePath,
+  getEslintCachePath,
+};
