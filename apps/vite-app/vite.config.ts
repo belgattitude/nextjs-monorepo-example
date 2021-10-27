1865665a9ae3b12f23d4068818cb34edb991b8bc
Sébastien Vanvelthem <belgattitude@gmail.com>
2021-10-27 19:30:39 +0200

    fix(lint): run lint --fix on vite.config.ts

--- apps/vite-app/vite.config.ts
+++ apps/vite-app/vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
+import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 import { compilerOptions } from './tsconfig.json';
-import { resolve } from 'path';
 
 // Workaround to enable tsconfig paths
 const alias = Object.entries(compilerOptions.paths).reduce(
