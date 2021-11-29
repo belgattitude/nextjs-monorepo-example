2205d253c2f938559db7c72c663f720c269120f0
Sébastien Vanvelthem <belgattitude@gmail.com>
2021-11-29 20:58:09 +0100

    chore: improve output

--- apps/web-app/playwright.config.ts
+++ apps/web-app/playwright.config.ts
@@ -2,20 +2,35 @@ import path from 'path';
 import type { PlaywrightTestConfig } from '@playwright/test';
 import { devices } from '@playwright/test';
 
-const skipNextJsBuild = process.env.E2E_SKIP_NEXTJS_BUILD;
+const skipNextJsBuild = process.env.E2E_SKIP_NEXTJS_BUILD === '1';
+const outputDir = path.join(__dirname, 'e2e/.out');
 
 // Reference: https://playwright.dev/docs/test-configuration
 const config: PlaywrightTestConfig = {
   testDir: path.join(__dirname, 'e2e'),
   timeout: 30_000,
-  retries: 2,
+  retries: 1,
   // Artifacts folder where screenshots, videos, and traces are stored.
-  outputDir: 'e2e/.out/test-results/',
+  outputDir: outputDir,
+  preserveOutput: 'always',
+  reporter: [
+    process.env.CI ? ['github'] : ['list'],
+    ['json', { outputFile: `${outputDir}/test-results.json` }],
+    [
+      'html',
+      {
+        outputFolder: `${outputDir}/html`,
+        open: process.env.CI ? 'never' : 'on-failure',
+      },
+    ],
+  ],
 
   // https://playwright.dev/docs/test-advanced#launching-a-development-web-server-during-the-tests
 
   webServer: {
-    command: `${skipNextJsBuild ? 'yarn build && ' : ''} yarn start -p 3000`,
+    command: `${
+      skipNextJsBuild ? '' : 'yarn build && '
+    } yarn start -p 3000`.trim(),
     port: 3000,
     timeout: 120_000,
     reuseExistingServer: !process.env.CI,
