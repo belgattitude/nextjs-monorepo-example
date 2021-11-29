d20cdaf5b2745745acb5875eb66d84c70d45b96b
Sébastien Vanvelthem <belgattitude@gmail.com>
2021-11-29 17:49:53 +0100

    chore: configure playwright

---
+++ apps/web-app/playwright.config.ts
@@ -0,0 +1,63 @@
+import path from 'path';
+import type { PlaywrightTestConfig } from '@playwright/test';
+import { devices } from '@playwright/test';
+
+// Reference: https://playwright.dev/docs/test-configuration
+const config: PlaywrightTestConfig = {
+  testDir: path.join(__dirname, 'e2e'),
+  timeout: 30_000,
+  retries: 2,
+  // Artifacts folder where screenshots, videos, and traces are stored.
+  outputDir: 'e2e/.out/test-results/',
+
+  // https://playwright.dev/docs/test-advanced#launching-a-development-web-server-during-the-tests
+  webServer: {
+    command: 'yarn build && yarn start -p 3000',
+    port: 3000,
+    timeout: 120_000,
+    reuseExistingServer: !process.env.CI,
+  },
+
+  use: {
+    // Retry a test if its failing with enabled tracing. This allows you to analyse the DOM, console logs, network traffic etc.
+    // More information: https://playwright.dev/docs/trace-viewer
+    trace: 'retry-with-trace',
+
+    contextOptions: {
+      ignoreHTTPSErrors: true,
+    },
+  },
+
+  projects: [
+    {
+      name: 'Desktop Chrome',
+      use: {
+        ...devices['Desktop Chrome'],
+      },
+    },
+    // {
+    //   name: 'Desktop Firefox',
+    //   use: {
+    //     ...devices['Desktop Firefox'],
+    //   },
+    // },
+    // {
+    //   name: 'Desktop Safari',
+    //   use: {
+    //     ...devices['Desktop Safari'],
+    //   },
+    // },
+    // Test against mobile viewports.
+    {
+      name: 'Mobile Chrome',
+      use: {
+        ...devices['Pixel 5'],
+      },
+    },
+    {
+      name: 'Mobile Safari',
+      use: devices['iPhone 12'],
+    },
+  ],
+};
+export default config;
