aebc56f710b3288991ab77250ace68be0635d25a
Sébastien Vanvelthem <belgattitude@gmail.com>
2021-12-01 19:43:09 +0100

    test(e2e): fix test

--- apps/web-app/e2e/features/demo/home.spec.ts
+++ apps/web-app/e2e/features/demo/home.spec.ts
@@ -1,7 +1,16 @@
 import { test, expect } from '@playwright/test';
-import homeJson from '../../../public/locales/en/demo.json';
+import homeJsonEn from '../../../public/locales/en/demo.json';
+import homeJsonFr from '../../../public/locales/fr/demo.json';
 
-test('should navigate to the home page', async ({ page }) => {
-  await page.goto('/');
-  await expect(page.locator('title')).toContainText(homeJson.page.title);
+test.describe('Demo page', () => {
+  test('should have the title in english by default', async ({ page }) => {
+    await page.goto('/');
+    const title = await page.title();
+    expect(title).toBe(homeJsonEn.page.title);
+  });
+  test('should have the title in french', async ({ page }) => {
+    await page.goto('/fr');
+    const title = await page.title();
+    expect(title).toBe(homeJsonFr.page.title);
+  });
 });
