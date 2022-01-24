46f4652ffeac451e137a14cbc109b332e067c5e4
Sébastien Vanvelthem <belgattitude@gmail.com>
2022-01-24 15:49:41 +0100

    test: refactor e2e now that playwright handles ts aliases

--- apps/web-app/e2e/pages/index/index.spec.ts
+++ apps/web-app/e2e/pages/index/index.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
-import homeJsonEn from '../../../public/locales/en/demo.json';
-import homeJsonFr from '../../../public/locales/fr/demo.json';
+import homeJsonEn from '@/public/locales/en/demo.json';
+import homeJsonFr from '@/public/locales/fr/demo.json';
 
 test.describe('Demo page', () => {
   test('should have the title in english by default', async ({ page }) => {
