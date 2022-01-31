cd3113455aab5c6e5fc7ebdeac840dbdf6ffe3ec
Sébastien Vanvelthem <belgattitude@gmail.com>
2022-01-31 16:01:08 +0100

    feat: testing lint-staged

--- apps/vite-app/lint-staged.config.js
+++ apps/vite-app/lint-staged.config.js
@@ -1,2 +1,29 @@
-const config = require('../../lint-staged.config.base');
-module.exports = config;
+// @ts-check
+
+const {
+  concatFilesForPrettier,
+  getEslintFixCmd,
+} = require('../../lint-staged.common.js');
+
+/**
+ * @type {Record<string, (filenames: string[]) => string | string[] | Promise<string | string[]>>}
+ */
+const rules = {
+  '**/*.{js,jsx,ts,tsx}': (filenames) => {
+    return getEslintFixCmd({
+      cwd: __dirname,
+      fix: true,
+      cache: true,
+      // when autofixing staged-files a good tip is to disable react-hooks/exhaustive-deps, cause
+      // a change here can potentially break things without proper visibility.
+      rules: ['react-hooks/exhaustive-deps: off'],
+      maxWarnings: 25,
+      files: filenames,
+    });
+  },
+  '**/*.{json,md,mdx,css,html,yml,yaml,scss}': (filenames) => {
+    return [`prettier --write ${concatFilesForPrettier(filenames)}`];
+  },
+};
+
+module.exports = rules;
--- apps/web-app/lint-staged.config.js
+++ apps/web-app/lint-staged.config.js
@@ -14,24 +14,12 @@ const rules = {
       cwd: __dirname,
       fix: true,
       cache: true,
-      // when autofixing staged-files a good tip is to disable react-hooks/exhaustive-deps, cause a
+      // when autofixing staged-files a good tip is to disable react-hooks/exhaustive-deps, cause
       // a change here can potentially break things without proper visibility.
       rules: ['react-hooks/exhaustive-deps: off'],
       maxWarnings: 25,
       files: filenames,
     });
-    /*
-    return [
-      // react-hooks/exhaustive-deps must be kept off, a change made here can potentially break your code
-      `${getYarnCmdInWorkingDir(
-        __dirname
-      )} eslint --rule 'react-hooks/exhaustive-deps: off' --max-warnings=25 --cache --fix ${filenames
-        .map((f) => path.relative(__dirname, f))
-        .map((f) => `"${f}"`)
-        .join(' ')}`,
-    ];
-
-     */
   },
   '**/*.{json,md,mdx,css,html,yml,yaml,scss}': (filenames) => {
     return [`prettier --write ${concatFilesForPrettier(filenames)}`];
