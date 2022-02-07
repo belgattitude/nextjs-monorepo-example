d1c2aa53b3bc3bcbe2985b8d6b3438bfdc0fab67
Sébastien Vanvelthem <belgattitude@gmail.com>
2022-02-07 22:07:05 +0100

    chore(eslint): add eslint:recommended

--- .eslintrc.base.js
+++ .eslintrc.base.js
@@ -24,6 +24,7 @@ module.exports = {
     },
   },
   extends: [
+    'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:import/recommended',
     'plugin:import/typescript',
