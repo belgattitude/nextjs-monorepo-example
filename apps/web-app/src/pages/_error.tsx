697527c899159f4f0a08abdf54f78c4209c00e43
Sébastien Vanvelthem <belgattitude@gmail.com>
2022-02-23 18:12:04 +0100

    feat: support sentry

--- apps/web-app/src/pages/_error.tsx
+++ apps/web-app/src/pages/_error.tsx
@@ -3,11 +3,15 @@
  * @link https://nextjs.org/docs/advanced-features/custom-error-page
  */
 
-import * as Sentry from '@sentry/node';
-import { isNonEmptyString } from '@your-org/core-lib';
+import {
+  captureException as sentryCaptureException,
+  flush as sentryFlush,
+} from '@sentry/nextjs';
 import type { NextPage, NextPageContext } from 'next';
 import NextErrorComponent from 'next/error';
 import type { ErrorProps } from 'next/error';
+
+const sentryIgnoredStatusCodes: number[] = [404, 410];
 
 // Adds HttpException to the list of possible error types.
 type AugmentedError = NonNullable<NextPageContext['err']> | null;
@@ -21,43 +25,15 @@ type AugmentedNextPageContext = Omit<NextPageContext, 'err'> & {
   err: AugmentedError;
 };
 
-const captureException = async (err: string | Error) => {
-  if (
-    process.env.NEXT_PUBLIC_SENTRY_DSN &&
-    isNonEmptyString(process.env.NEXT_PUBLIC_SENTRY_DSN)
-  ) {
-    Sentry.captureException(err);
-  }
-};
-
-const captureExceptionAndFlush = async (
-  err: string | Error,
-  flushAfter = 2000
-) => {
-  if (
-    process.env.NEXT_PUBLIC_SENTRY_DSN &&
-    isNonEmptyString(process.env.NEXT_PUBLIC_SENTRY_DSN)
-  ) {
-    Sentry.captureException(err);
-    if (flushAfter > 0) {
-      // Flushing before returning is necessary if deploying to Vercel, see
-      // https://vercel.com/docs/platform/limits#streaming-responses
-      await Sentry.flush(flushAfter);
-    }
-  }
-};
-
 const CustomError: NextPage<CustomErrorProps> = (props) => {
   const { statusCode, err, hasGetInitialPropsRun } = props;
 
   if (!hasGetInitialPropsRun && err) {
     // getInitialProps is not called in case of https://github.com/vercel/next.js/issues/8592.
     // As a workaround, we pass err via _app.js so it can be captured
-
+    sentryCaptureException(err);
     // Flushing is not required in this case as it only happens on the client
-    captureException(err);
   }
-
   return <NextErrorComponent statusCode={statusCode} />;
 };
 
@@ -75,12 +51,20 @@ CustomError.getInitialProps = async ({
   // getInitialProps has run
   errorInitialProps.hasGetInitialPropsRun = true;
 
+  // Returning early because we don't want to log ignored errors to Sentry.
+  if (
+    typeof res?.statusCode === 'number' &&
+    sentryIgnoredStatusCodes.includes(res.statusCode)
+  ) {
+    return errorInitialProps;
+  }
+
   // Running on the server, the response object (`res`) is available.
   //
   // Next.js will pass an err on the server if a page's data fetching methods
   // threw or returned a Promise that rejected
   //
-  // Running on the client (browser), Next.js will provide an err if:
+  // Running on the client (browser), Next.js will provide an error if:
   //
   //  - a page's `getInitialProps` threw or returned a Promise that rejected
   //  - an exception was thrown somewhere in the React lifecycle (render,
@@ -89,22 +73,22 @@ CustomError.getInitialProps = async ({
   //    Boundaries: https://reactjs.org/docs/error-boundaries.html
 
   if (err) {
+    sentryCaptureException(err);
+
     // Flushing before returning is necessary if deploying to Vercel, see
     // https://vercel.com/docs/platform/limits#streaming-responses
-    await captureExceptionAndFlush(err, 2000);
+    await sentryFlush(2000);
+
     return errorInitialProps;
   }
 
   // If this point is reached, getInitialProps was called without any
   // information about what the error might be. This is unexpected and may
   // indicate a bug introduced in Next.js, so record it in Sentry
-  //
-  // Flushing before returning is necessary if deploying to Vercel, see
-  // https://vercel.com/docs/platform/limits#streaming-responses
-  await captureExceptionAndFlush(
-    new Error(`_error.js getInitialProps missing data at path: ${asPath}`),
-    2000
+  sentryCaptureException(
+    new Error(`_error.js getInitialProps missing data at path: ${asPath}`)
   );
+  await sentryFlush(2000);
 
   return errorInitialProps;
 };
