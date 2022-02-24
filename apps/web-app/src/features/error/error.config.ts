c75e3448e580c518afa927d376f34b7e0f174c96
Sébastien Vanvelthem <belgattitude@gmail.com>
2022-02-24 21:36:50 +0100

    fix: i18n namespace for errors

--- apps/web-app/src/features/error/error.config.ts
+++ apps/web-app/src/features/error/error.config.ts
@@ -3,9 +3,9 @@ import type { I18nActiveNamespaces } from '@/core/i18n/i18n-namespaces.type';
 export type ErrorConfig = {
   // Define installed namespaces in the type here
   // to allow full typechecking of your translation keys.
-  i18nNamespaces: Readonly<I18nActiveNamespaces<'common'>>;
+  i18nNamespaces: Readonly<I18nActiveNamespaces<'system'>>;
 };
 
 export const errorConfig: ErrorConfig = {
-  i18nNamespaces: ['common'],
+  i18nNamespaces: ['system'],
 } as const;
