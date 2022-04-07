464dc34ea7e5ad63b95cde125f48a5ef74b9517b
Sébastien Vanvelthem <belgattitude@gmail.com>
2022-04-07 22:42:47 +0200

    fix: seeds for planetscale

--- packages/db-main-prisma/prisma/seed.ts
+++ packages/db-main-prisma/prisma/seed.ts
@@ -17,6 +17,7 @@ const userData: Prisma.UserCreateInput[] = [
           title: 'Nextjs monorepo example',
           slug: 'first-post',
           link: 'https://github.com/belgattitude/nextjs-monorepo-example',
+          content: 'Hello world',
           image:
             'https://images.unsplash.com/photo-1625904835711-fa25795530e8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80',
           publishedAt: new Date(),
