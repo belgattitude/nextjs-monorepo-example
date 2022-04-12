9cb1bab3abc858086c2df1caebe7173a22de546b
Mark Q Schwanda <mqschwanda@gmail.com>
2022-04-12 03:42:12 -0400

    refactor(pages): cleanup pages

--- apps/web-app/e2e/pages/_monitor/sentry.spec.ts
+++ apps/web-app/e2e/pages/_monitor/sentry.spec.ts
@@ -4,16 +4,14 @@ test.describe('Sentry monitor pages', () => {
   test.describe('Client-side rendered', () => {
     test('should have a title containing error', async ({ page }) => {
       await page.goto('/_monitor/sentry/csr-page');
-      await expect(page).toHaveTitle('');
-      // await expect(page).toHaveTitle(/error/i);
+      await expect(page).toHaveTitle(/error/i);
     });
   });
 
   test.describe('Server-side rendered', () => {
     test('should have a title containing error', async ({ page }) => {
       await page.goto('/_monitor/sentry/ssr-page');
-      await expect(page).toHaveTitle('');
-      // await expect(page).toHaveTitle(/error/i);
+      await expect(page).toHaveTitle(/error/i);
     });
   });
 });
