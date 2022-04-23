d136816dbf5d4b42ebca677d283917b7460452da
Sébastien Vanvelthem <belgattitude@gmail.com>
2022-04-23 17:09:01 +0200

    feat: add debug i18n function

--- apps/nextjs-app/next-i18next.config.js
+++ apps/nextjs-app/next-i18next.config.js
@@ -1,11 +1,16 @@
 const path = require('path');
 
+const defaultLocale = 'en';
+const debugI18n = ['true', 1].includes(
+  process.env?.NEXTJS_DEBUG_I18N ?? 'false'
+);
+
 /**
  * @type {import('next-i18next').UserConfig}
  */
 module.exports = {
   i18n: {
-    defaultLocale: 'en',
+    defaultLocale,
     locales: ['en', 'fr'],
   },
   strictMode: true,
@@ -13,6 +18,12 @@ module.exports = {
   react: {
     useSuspense: false,
   },
+  debug: debugI18n,
+  /*
+  interpolation: {
+    escapeValue: false,
+  },
+  */
   localePath:
     typeof window === 'undefined'
       ? path.resolve('../../packages/common-i18n/src/locales')
