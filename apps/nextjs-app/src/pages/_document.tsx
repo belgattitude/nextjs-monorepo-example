97ec596f9c6a81ded230d50b0f78d5a0be7b8d23
Sébastien Vanvelthem <belgattitude@gmail.com>
2022-04-23 16:40:31 +0200

    feat: add html lang from i18next

--- apps/nextjs-app/src/pages/_document.tsx
+++ apps/nextjs-app/src/pages/_document.tsx
@@ -57,8 +57,10 @@ class MyDocument extends Document<Props> {
   }
 
   render() {
+    const { locale } = this.props.__NEXT_DATA__;
+
     return (
-      <Html>
+      <Html lang={locale}>
         <Head>
           <meta charSet="utf-8" />
           <link
