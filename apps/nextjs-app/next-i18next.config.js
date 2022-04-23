7383fb527b1ae626f0a5ceac7876bb0191963d95
Sébastien Vanvelthem <belgattitude@gmail.com>
2022-04-23 16:17:19 +0200

    feat: improve i18n support

--- apps/nextjs-app/next-i18next.config.js
+++ apps/nextjs-app/next-i18next.config.js
@@ -1,13 +1,20 @@
 const path = require('path');
 
+/**
+ * @type {import('next-i18next').UserConfig}
+ */
 module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'fr'],
   },
+  strictMode: true,
   reloadOnPrerender: process.env.NODE_ENV === 'development',
   react: {
     useSuspense: false,
   },
-  localePath: path.resolve('../../packages/common-i18n/src/locales'),
+  localePath:
+    typeof window === 'undefined'
+      ? path.resolve('../../packages/common-i18n/src/locales')
+      : undefined,
 };
