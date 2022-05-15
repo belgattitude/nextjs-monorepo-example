2dd1be90b40658b1cf6bc3046b789f8df3f13b07
Sébastien Vanvelthem <belgattitude@gmail.com>
2022-05-15 15:05:41 +0200

    chore: attempt to fix remix on vercel

--- apps/remix-app/api/index.js
+++ apps/remix-app/api/index.js
@@ -1,6 +1,8 @@
 // @ts-check
+const path = require('path');
+
 const { createRequestHandler } = require('@remix-run/vercel');
 
 module.exports = createRequestHandler({
-  build: require('./_build'),
+  build: require(path.resolve(__dirname, '_build')),
 });
