e51d74463c693cb2384a198ffa3a91f449fc4370
Sébastien Vanvelthem <belgattitude@gmail.com>
2022-05-15 11:48:25 +0200

    test: fix grammar

--- apps/nextjs-app/src/features/system/pages/__tests__/ErrorPage.test.tsx
+++ apps/nextjs-app/src/features/system/pages/__tests__/ErrorPage.test.tsx
@@ -2,7 +2,7 @@ import { ErrorPage } from '@/features/system/pages';
 import { render, screen } from '@/test-utils';
 
 describe('ErrorPage test', () => {
-  it('should contain error status code', async () => {
+  it('should contain error passed status code', async () => {
     render(<ErrorPage statusCode={500} />);
     expect(screen.getByTestId('error-status-code')).toHaveTextContent('500');
   });
--- apps/nextjs-app/src/features/system/pages/__tests__/NotFoundPage.test.tsx
+++ apps/nextjs-app/src/features/system/pages/__tests__/NotFoundPage.test.tsx
@@ -2,7 +2,7 @@ import { NotFoundPage } from '@/features/system/pages';
 import { render, screen } from '@/test-utils';
 
 describe('NotFoundPage test', () => {
-  it('should contains children', async () => {
+  it('should contain passed title', async () => {
     render(<NotFoundPage title={'404 - Not found'} />);
     expect(screen.getByTestId('not-found-title')).toHaveTextContent(
       '404 - Not found'
