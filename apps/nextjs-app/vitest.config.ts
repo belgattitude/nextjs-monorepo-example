76cbf1f1c9fdc97b252019da72f8a14a4c965576
Sébastien Vanvelthem <belgattitude@gmail.com>
2022-05-15 11:49:35 +0200

    chore: with vitest example

---
+++ apps/nextjs-app/vitest.config.ts
@@ -0,0 +1,32 @@
+/// <reference types="vitest" />
+
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+      babel: {
+        plugins: ['@emotion/babel-plugin'],
+      },
+    }),
+    tsconfigPaths(),
+    svgr({
+      // Set it to `true` to export React component as default.
+      // Notice that it will override the default behavior of Vite.
+      exportAsDefault: true,
+      // svgr options: https://react-svgr.com/docs/options/
+      svgrOptions: {},
+    }),
+  ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    passWithNoTests: false,
+    exclude: ['**/e2e/**'],
+  },
+});
