778117c277c49123dc0de8f4b27b46c957e113b5
Sébastien Vanvelthem <belgattitude@gmail.com>
2022-05-29 23:42:44 +0200

    fix: remix deploy on vercel

--- apps/remix-app/api/index.js
+++
@@ -1,8 +0,0 @@
-// @ts-check
-const path = require('path');
-
-const { createRequestHandler } = require('@remix-run/vercel');
-
-module.exports = createRequestHandler({
-  build: require(path.resolve(__dirname, '_build')),
-});
