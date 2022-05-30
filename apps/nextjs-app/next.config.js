ac7f7a6efbe6d13cacc0c86906248de1850e9577
Sébastien Vanvelthem <belgattitude@gmail.com>
2022-05-30 19:00:42 +0200

    chore: test if BrowserTracing is tree-shaken

--- apps/nextjs-app/next.config.js
+++ apps/nextjs-app/next.config.js
@@ -210,6 +210,8 @@ const nextConfig = {
     config.plugins.push(
       new webpack.DefinePlugin({
         __SENTRY_DEBUG__: NEXTJS_SENTRY_DEBUG,
+        // Should tree-shake sentry BrowserTracing.
+        __SENTRY_TRACING__: false,
       })
     );
 
