9ae7d7dd4d5b118937025d77ee31ecd099790903
Sébastien Vanvelthem <belgattitude@gmail.com>
2022-07-02 22:20:20 +0200

    fix: classname order

--- apps/remix-app/src/features/system/pages/NotFoundPage.tsx
+++ apps/remix-app/src/features/system/pages/NotFoundPage.tsx
@@ -9,14 +9,14 @@ export const NotFoundPage: FC<Props> = (props) => {
   const title = props.title || 'Not Found';
   return (
     <>
-      <div className="flex flex-col justify-center items-center w-screen h-screen bg-white">
+      <div className="flex h-screen w-screen flex-col items-center justify-center bg-white">
         <h1
           data-testid="not-found-title"
           className="text-5xl text-black md:text-4xl lg:text-5xl"
         >
           {title}
         </h1>
-        <p className="mt-5 text-xl text-center no-underline hover:underline">
+        <p className="mt-5 text-center text-xl no-underline hover:underline">
           <a href={'/'}>Back to home</a>
         </p>
       </div>
