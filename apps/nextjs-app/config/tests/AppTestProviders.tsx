398e737a6c39a55eff40cf8c88cc7fe3d24ed041
Sébastien Vanvelthem <belgattitude@gmail.com>
2022-09-17 19:01:22 +0200

    feat: with next-auth, add provider

--- apps/nextjs-app/config/tests/AppTestProviders.tsx
+++ apps/nextjs-app/config/tests/AppTestProviders.tsx
@@ -1,10 +1,20 @@
-import type { FC, ReactNode } from 'react';
+import type { Session } from 'next-auth';
+import type { FC, PropsWithChildren } from 'react';
 import { AppProviders } from '../../src/AppProviders';
 import { I18nextTestStubProvider } from './I18nextTestStubProvider';
 
-export const AppTestProviders: FC<{ children: ReactNode }> = ({ children }) => {
+const fakeNextAuthSession: Session = {
+  user: {
+    email: 'test@example.com',
+    role: 'guest',
+    name: 'AppTestProvider',
+  },
+  expires: '2050-01-01T00:00:00.000Z',
+};
+
+export const AppTestProviders: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <AppProviders>
+    <AppProviders session={fakeNextAuthSession}>
       <I18nextTestStubProvider>{children}</I18nextTestStubProvider>
     </AppProviders>
   );
