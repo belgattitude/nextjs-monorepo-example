d96a3498ca5f5768070bc79be75dd55768452fa9
Sébastien Vanvelthem <belgattitude@gmail.com>
2022-09-17 19:05:27 +0200

    fix: lint import order

--- packages/common-i18n/src/I18nNamespaces.ts
+++ packages/common-i18n/src/I18nNamespaces.ts
@@ -1,8 +1,8 @@
 import type admin from './locales/en/admin.json';
 import type auth from './locales/en/auth.json';
 import type blog from './locales/en/blog.json';
-import type demo from './locales/en/demo.json';
 import type common from './locales/en/common.json';
+import type demo from './locales/en/demo.json';
 import type home from './locales/en/home.json';
 import type navigation from './locales/en/navigation.json';
 import type system from './locales/en/system.json';
