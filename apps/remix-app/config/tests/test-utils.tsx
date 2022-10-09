517b47ed1ce7610fd935d54f81da95d82e7a4f7f
Sébastien Vanvelthem <belgattitude@gmail.com>
2022-10-09 09:16:42 +0200

    chore: align and rework test configs

--- apps/remix-app/config/tests/test-utils.tsx
+++ apps/remix-app/config/tests/test-utils.tsx
@@ -3,15 +3,19 @@
  * @see https://testing-library.com/docs/react-testing-library/setup#configuring-jest-with-test-utils
  */
 import { render } from '@testing-library/react';
-import type React from 'react';
-import { AppTestProviders } from './app-test-providers';
+import type { FC, PropsWithChildren, ReactElement } from 'react';
+
+const AppTestProviders: FC<PropsWithChildren> = (props) => (
+  <>{props.children}</>
+);
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const customRender = (ui: React.ReactElement, options?: any) =>
+const customRender = (ui: ReactElement, options?: any) =>
   render(ui, { wrapper: AppTestProviders, ...options });
 
 // re-export everything
 export * from '@testing-library/react';
+export { default as userEvent } from '@testing-library/user-event';
 
 // override render method
 export { customRender as render };
