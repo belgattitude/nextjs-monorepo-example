a5a1e35193417beaf01dc355983103278a40426b
Sébastien Vanvelthem <belgattitude@gmail.com>
2022-11-15 16:18:07 +0100

    feat: add bodyParser config

--- apps/nextjs-app/src/pages/api/gateway/graphql.ts
+++ apps/nextjs-app/src/pages/api/gateway/graphql.ts
@@ -2,6 +2,14 @@ import { getBuiltMesh } from '@your-org/api-gateway';
 import { createYoga } from 'graphql-yoga';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
+// Docs: https://vercel.com/docs/concepts/functions/serverless-functions
+export const config = {
+  api: {
+    // Disable body parsing (required for file uploads)
+    bodyParser: false,
+  },
+};
+
 async function buildServer() {
   const mesh = await getBuiltMesh();
   return createYoga({
