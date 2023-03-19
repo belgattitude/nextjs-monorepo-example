5c8d5a4f86cc50c07fb83dff31885b5e9219b996
Sébastien Vanvelthem <belgattitude@gmail.com>
2023-03-19 13:10:39 +0100

    feat: move to storybook 7rc + vite + ts config paths

--- packages/ui-lib/.storybook/main.ts
+++ packages/ui-lib/.storybook/main.ts
@@ -1,14 +1,21 @@
 import type { StorybookConfig } from '@storybook/react-vite';
+
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    //'@storybook/addon-interactions',
+    // Not working in storybook v7 + vite
+    // '@storybook/addon-interactions',
   ],
   framework: {
     name: '@storybook/react-vite',
-    options: {},
+    options: {
+      strictMode: true,
+      builder: {
+        viteConfigPath: './vite.config.storybook.ts',
+      },
+    },
   },
   docs: {
     autodocs: 'tag',
