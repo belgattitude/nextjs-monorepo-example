192b3a3101a62d38e74f1eb976f26d40221923b8
Sébastien Vanvelthem <belgattitude@users.noreply.github.com>
2023-03-30 16:08:21 +0200

    feat: remix vercel new integration (#3586)

--- apps/remix-app/remix.config.js
+++ apps/remix-app/remix.config.js
@@ -4,17 +4,7 @@
 module.exports = {
   appDirectory: 'src',
   ignoredRouteFiles: ['**/.*'],
-  serverBuildTarget: 'vercel',
-  // When running locally in development mode, we use the built in remix
-  // server. This does not understand the vercel lambda module format,
-  // so we default back to the standard build output.
-  server:
-    process.env.NODE_ENV === 'development'
-      ? undefined
-      : './src/server/vercel.js',
-
-  // From 1.5.0 defaults
-  assetsBuildDirectory: 'public/build',
-  serverBuildPath: 'api/index.js',
-  publicPath: '/build/',
+  // assetsBuildDirectory: "public/build",
+  // serverBuildPath: "build/index.js",
+  // publicPath: "/build/",
 };
