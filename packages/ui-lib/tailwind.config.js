c2ad56aeb24968e25a22fcc757239ee59d183c90
Sébastien Vanvelthem <belgattitude@users.noreply.github.com>
2023-03-30 12:13:56 +0200

    chore(deps): remove obsolete tailwind line clamp (#3578)

--- packages/ui-lib/tailwind.config.js
+++ packages/ui-lib/tailwind.config.js
@@ -60,8 +60,5 @@ module.exports = {
        */
     },
   },
-  plugins: [
-    require('@tailwindcss/line-clamp'),
-    require('@tailwindcss/aspect-ratio'),
-  ],
+  plugins: [require('@tailwindcss/aspect-ratio')],
 };
