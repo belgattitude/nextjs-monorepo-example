c8fe35a286cecf0782fac4d7e9e7e436f53c4dee
Marcelo Santana Junior <juniorsantanamarcelo@gmail.com>
2023-04-01 01:30:32 -0300

    fix(deps): banner

--- apps/nextjs-app/src/components/banner/Banner.tsx
+++ apps/nextjs-app/src/components/banner/Banner.tsx
@@ -17,7 +17,7 @@ export const Banner: FC<Props> = () => {
             <p className="ml-3 truncate font-medium text-white">
               <span className="md:hidden">We announced a new product!</span>
               <span className="hidden md:inline">
-                Big news! We're excited to announce a brand new product.
+                Big news! We're excited to announce a brand new product.2
               </span>
             </p>
           </div>
