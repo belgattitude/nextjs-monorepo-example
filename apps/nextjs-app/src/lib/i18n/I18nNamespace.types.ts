e70ba1c3467dfdb7b8c580e6172f7d2d898dcf72
Sébastien Vanvelthem <belgattitude@gmail.com>
2023-06-15 16:45:10 +0200

    refactor: upgrade to i18next 23

--- apps/nextjs-app/src/lib/i18n/I18nNamespace.types.ts
+++ apps/nextjs-app/src/lib/i18n/I18nNamespace.types.ts
@@ -1,7 +1,5 @@
-import type { CustomTypeOptions, Namespace } from 'i18next';
+import type { CustomTypeOptions } from 'i18next';
 
 export type I18nNamespace = keyof CustomTypeOptions['resources'];
 
-type ArrayElementOrSelf<T> = T extends Array<infer U> ? U[] : T[];
-
-export type I18nActiveNamespaces = ArrayElementOrSelf<Namespace>;
+export type I18nActiveNamespaces = I18nNamespace[];
