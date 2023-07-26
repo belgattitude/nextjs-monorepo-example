af14ad3c0c09baac781730552936b2f2ac6e421c
Sébastien Vanvelthem <belgattitude@users.noreply.github.com>
2023-07-26 21:01:09 +0200

    Graphql jit (#4146)
    * chore: env example for prod
    
    * chore(docker): no need for libc6-compat
    
    * feat(graphql): perf & security
    
    * docs: improved readme
    
    * chore(deps): to latest

--- apps/nextjs-app/src/pages/api/graphql/index.ts
+++ apps/nextjs-app/src/pages/api/graphql/index.ts
@@ -1,3 +1,11 @@
+import { useGraphQlJit } from '@envelop/graphql-jit';
+import { useValidationCache } from '@envelop/validation-cache';
+import { characterLimitPlugin } from '@escape.tech/graphql-armor-character-limit';
+import { costLimitPlugin } from '@escape.tech/graphql-armor-cost-limit';
+import { maxAliasesPlugin } from '@escape.tech/graphql-armor-max-aliases';
+import { maxDepthPlugin } from '@escape.tech/graphql-armor-max-depth';
+import { maxDirectivesPlugin } from '@escape.tech/graphql-armor-max-directives';
+import { maxTokensPlugin } from '@escape.tech/graphql-armor-max-tokens';
 import { createYoga } from 'graphql-yoga';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { graphqlSchema } from '@/backend/graphql/graphqlSchema';
@@ -17,4 +25,44 @@ export default createYoga<{
   schema: graphqlSchema,
   graphqlEndpoint: '/api/graphql',
   graphiql: true,
+  plugins: [
+    useGraphQlJit(),
+    useValidationCache(),
+    characterLimitPlugin(
+      // // @link https://escape.tech/graphql-armor/docs/plugins/character-limit
+      {
+        maxLength: 15_000,
+      }
+    ),
+    costLimitPlugin(
+      // @link https://escape.tech/graphql-armor/docs/plugins/cost-limit
+      {
+        maxCost: 5_000,
+        objectCost: 2,
+        scalarCost: 1,
+        depthCostFactor: 1.5,
+        ignoreIntrospection: true,
+      }
+    ),
+    maxDepthPlugin({
+      // @link https://escape.tech/graphql-armor/docs/plugins/max-depth
+      n: 6,
+    }),
+    maxDirectivesPlugin(
+      // @link https://escape.tech/graphql-armor/docs/plugins/max-directives
+      {
+        n: 50,
+      }
+    ),
+    maxTokensPlugin(
+      // @link https://escape.tech/graphql-armor/docs/plugins/max-tokens
+      {
+        n: 1_000,
+      }
+    ),
+    maxAliasesPlugin({
+      // @link https://escape.tech/graphql-armor/docs/plugins/max-aliases
+      n: 15,
+    }),
+  ],
 });
