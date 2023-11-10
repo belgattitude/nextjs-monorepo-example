f746f791b8c8d540a55ebd5bd0594657a2965101
Sébastien Vanvelthem <belgattitude@gmail.com>
2023-11-10 19:04:12 +0100

    refactor: prisma extracted

--- packages/db-main-prisma/src/lib/AbstractSeed.ts
+++ packages/db-main-prisma/src/lib/AbstractSeed.ts
@@ -1,4 +1,4 @@
-import type { PrismaClient } from '@prisma/client';
+import type { PrismaClient } from '../generated/client';
 
 export abstract class AbstractSeed {
   constructor(public prisma: PrismaClient) {}
