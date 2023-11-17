1294e83a511bbdcaf107dbb22482ab07df920964
Sébastien Vanvelthem <belgattitude@users.noreply.github.com>
2023-11-17 18:37:57 +0100

    chore(deps): all to latest (#4666)

--- apps/nextjs-app/vitest.config.ts
+++ apps/nextjs-app/vitest.config.ts
@@ -4,7 +4,6 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
 
 const testFiles = ['./src/**/*.test.{js,jsx,ts,tsx}'];
-
 export default defineConfig({
   plugins: [
     react({
