61d58b1b60528f9b23e2afca996e72aaf42bedc3
Sébastien Vanvelthem <belgattitude@gmail.com>
2024-02-21 18:31:12 +0100

    chore(deps): all to latest

--- apps/remix-app/src/app/root.tsx
+++ apps/remix-app/src/app/root.tsx
@@ -1,31 +1,7 @@
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from '@remix-run/react';
+import type { LinksFunction } from '@remix-run/node';
 
-// https://remix.run/docs/en/main/future/vite#fix-up-css-imports
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import stylesheet from '../styles/tailwind.css';
 
-export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <LiveReload />
-        <Scripts />
-      </body>
-    </html>
-  );
-}
+export const links: LinksFunction = () => [
+  { rel: 'stylesheet', href: stylesheet },
+];
