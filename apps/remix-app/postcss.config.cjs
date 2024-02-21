511e41fd961ec7a81f4ddbb568ba4e2359cef9d9
Sébastien Vanvelthem <belgattitude@gmail.com>
2024-02-21 18:35:18 +0100

    chore(deps): all to latest

--- apps/remix-app/postcss.config.cjs
+++ apps/remix-app/postcss.config.cjs
@@ -6,6 +6,6 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
+    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
   },
 };
