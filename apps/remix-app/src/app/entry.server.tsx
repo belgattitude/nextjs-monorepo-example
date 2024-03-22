9d7505584e0399b6faad2609acc52ae7bd7bb37a
Sébastien Vanvelthem <belgattitude@gmail.com>
2024-03-22 11:25:05 +0100

    chore(deps): isbot upgrade

--- apps/remix-app/src/app/entry.server.tsx
+++ apps/remix-app/src/app/entry.server.tsx
@@ -6,7 +6,7 @@ import {
   type EntryContext,
 } from '@remix-run/node';
 import { RemixServer } from '@remix-run/react';
-import isbot from 'isbot';
+import { isbot } from 'isbot';
 import { renderToPipeableStream } from 'react-dom/server';
 
 const ABORT_DELAY = 5_000;
