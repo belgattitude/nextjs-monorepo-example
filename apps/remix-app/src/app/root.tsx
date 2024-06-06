5e141a42971b8c8923ba20f81394379ff577171d
Sébastien Vanvelthem <belgattitude@gmail.com>
2024-06-07 01:47:09 +0200

    refactor!: remove remix-app

--- apps/remix-app/src/app/root.tsx
+++
@@ -1,7 +0,0 @@
-import type { LinksFunction } from '@remix-run/node';
-
-import stylesheet from '../styles/tailwind.css?url';
-
-export const links: LinksFunction = () => [
-  { rel: 'stylesheet', href: stylesheet },
-];
